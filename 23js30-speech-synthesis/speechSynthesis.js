bb469a2dfd34cd41bac54c5736cac58053f97c62
stan <citrus_82@nextmail.ru>
2017-05-29 16:35:40 +0300

    Completed excercise #23

---
+++ 23js30-speech-synthesis/speechSynthesis.js
@@ -0,0 +1,50 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+
+msg.text = document.querySelector('[name="text"]').value;
+
+
+function populateVoices () {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    // if we want to filter out some languages
+    //.filter(voice => voice.lang.includes('en'))
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+}
+
+function setVoice () {
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle(); // restart/stop the speech
+}
+
+
+function toggle (startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg)
+  }
+}
+
+function setOption () {
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+  toggle();
+}
+
+// wait on voices to be loaded before fetching list
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+
+// we need to pass function 'toggle' & pass 'false' to toggle()
+// Method #1
+// so we use binding to 'null' context with argument===false
+// stopButton.addEventListener('click', toggle.bind(null, false));
+// Method #2: (arrow) inline function
+stopButton.addEventListener('click', () => toggle(false));
